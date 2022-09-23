2c998fa6abc8566bd5b0e9925bf856183cb7cacf
Darlene Matos <darlenem19@gmail.com>
2022-09-22 23:07:15 -0400

    Add home option to header

--- darlas-portfolio/src/components/Header.jsx
+++ darlas-portfolio/src/components/Header.jsx
@@ -5,9 +5,10 @@ const Header = () => {
 
 
             <li className="nav-item"> DM </li>
+            <li className="nav-item"> Home </li>
             <li className="nav-item"> About </li>
             <li className="nav-item"> Projects</li>
-            <li className="nav-item"> Contact </li>
+            <button className="nav-item call-to-action"> Contact </button>
 
 
         </navbar>
