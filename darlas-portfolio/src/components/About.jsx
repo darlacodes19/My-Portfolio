5c46b66494bef3508119aacee7ee2544d7f5b554
Darlene Matos <darlenem19@gmail.com>
2022-10-17 23:49:01 -0400

    Remove SVG wave

--- darlas-portfolio/src/components/About.jsx
+++ darlas-portfolio/src/components/About.jsx
@@ -2,8 +2,21 @@ const About = () => {
 
     return (
       <div  className="about-section">
+        <div className="about-me"> 
            <h2> About Me </h2>
+           <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        <div className="my-skills"> 
            <h2> Skills</h2>
+           <img src={require('../assets/html.png')} /> 
+           <p> HTML5 </p>
+
+           <img src={require('../assets/javaScript.png')} /> 
+
+        </div>
+           
+          
+           
       </div>
     )
 
