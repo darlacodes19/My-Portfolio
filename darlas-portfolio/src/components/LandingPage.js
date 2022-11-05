19b19eb8b35267640095dc3a769cd05ccc317d65
Darlene Matos <darlenem19@gmail.com>
2022-11-04 23:03:25 -0400

    Change class to className

--- darlas-portfolio/src/components/LandingPage.js
+++ darlas-portfolio/src/components/LandingPage.js
@@ -4,7 +4,7 @@ const LandingPage = () => {
       <div className="landing-container">
             <h3 className="landing-name"> Hi, I'm Darlene </h3>
             <h2 className="landing-title"> Front-End Developer</h2>
-            <div class="custom-shape-divider-bottom-1663259355">
+            <div className="custom-shape-divider-bottom-1663259355">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
     </svg>
