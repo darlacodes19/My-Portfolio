988f9ece82303c9587a3bcf30ae1c239660b27e1
Darlene Matos <darlenem19@gmail.com>
2022-12-03 14:21:13 -0500

    Add header and layout component within BrowserRouter tags to fix issue of website going blank after adding navlinks to the navbar

--- darlas-portfolio/src/App.js
+++ darlas-portfolio/src/App.js
@@ -1,12 +1,36 @@
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import About from "./components/About"
+import Contact from "./components/Contact"
+import LandingPage from "./components/LandingPage"
+import Projects from "./components/Projects"
+
+import Header from "./components/Header"
 import Layout from './Layout'
 import './App.css'
 
 function App() {
  
   return (
+    <BrowserRouter> 
     <div className="App">
-      <Layout /> 
+      {/* <Header />
+      <Layout />  */}
+
+      <Header /> 
+      <Layout />
+    
+        <Routes>
+           
+            <Route exact path="/" component={<LandingPage />} />
+            <Route exact path="/about" component={<About />} />  
+            <Route exact path="/projects" component={<Projects />} />
+            <Route exact path="/contact" component={<Contact />} /> 
+         </Routes> 
+         
+   
     </div>
+    </BrowserRouter>
   )
 }
 
